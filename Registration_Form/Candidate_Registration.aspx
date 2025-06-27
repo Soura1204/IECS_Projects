a8cffb28926877e7ff2592193a8088e1da8a8d1e
Sourajit Datta <dattasourajit04@gmail.com>
2025-06-27 17:08:02 +0530

    Registration Form

---
+++ Registration_Form/Candidate_Registration.aspx
@@ -0,0 +1,106 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Candidate_Registration.aspx.cs" Inherits="Registration_Form.Candidate_Registration" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+
+    <section id="main-content">
+
+    <section id="wrapper">
+        <div class="row">
+            <div class="col-lg-12">
+                <section class="panel">
+                    <header class="panel-heading">
+                        <div class="col-md-4 col-md-offset-4">
+                            <h1>Registration</h1>
+                        </div>
+                    </header>
+
+
+                    <div class="panel-body">
+                        <div class="row">
+                            <div class="col-md-4 col-md-offset-1">
+                                <div class="form-group">
+                                    <asp:Label Text="First Name" runat="server"  />
+                                    <asp:TextBox ID="txtFirstName" runat="server" Enabled="true" CssClass="form-control input-sm" placeholder="Enter your First Name" />
+                                </div>
+
+                                </div>
+
+                             <div class="col-md-4 col-md-offset-1">
+                             <div class="form-group">
+                <asp:Label Text="Last Name" runat="server"  />
+                <asp:TextBox ID="txtLastName" runat="server" Enabled="true" CssClass="form-control input-sm" placeholder="Enter your Last Name" />
+                 </div>
+                 </div>
+                  </div>
+
+
+                  <div class="row">
+                 <div class="col-md-4 col-md-offset-1">
+                    <div class="form-group">
+                        <asp:Label Text="Address" runat="server"  />
+                        <asp:TextBox ID="txtAddress" runat="server" Enabled="true" CssClass="form-control input-sm" placeholder="Enter your address" />
+                    </div>
+
+                   </div>
+
+                 <div class="col-md-4 col-md-offset-1">
+                 <div class="form-group">
+                    <asp:Label Text="DOB" runat="server"  />
+                    <asp:TextBox ID="txtDOB" runat="server" Enabled="true" TextMode="Date" CssClass="form-control input-sm" placeholder="DOB" />
+                </div>
+
+                </div>
+                </div>
+
+              <div class="row">
+            <div class="col-md-4 col-md-offset-1">
+            <div class="form-group">
+                <asp:Label Text="Email" runat="server"  />
+                 <asp:TextBox ID="txtEmail" runat="server" Enabled="true" CssClass="form-control input-sm" placeholder="Enter your email" />
+             </div>
+
+            </div>
+
+           <div class="col-md-4 col-md-offset-1">
+          <div class="form-group">
+           <asp:Label Text="Mobile Number" runat="server"  />
+          <asp:TextBox ID="txtMobile" runat="server" Enabled="true" CssClass="form-control input-sm" placeholder="Mobile Number 10 Digit Only" />
+          </div>
+           </div>
+            </div>
+            <div class="row">
+                <div class="col-md-8 col-md-offset-2">
+                    <div style="height: 15vh; display: flex; justify-content: center; align-items: center;">
+                    <asp:Button Text="SUBMIT" ID="btnsubmit" CssClass="btn btn-primary" Width="170px" runat="server" BackColor="#33CCFF" ForeColor="Black" OnClick="btnsubmit_Click" />
+                    <asp:Button Text="UPDATE" ID="btnsubmit1" CssClass="btn btn-primary" Width="170px" runat="server" BackColor="Lime" ForeColor="Black" OnClick="btnsubmit1_Click" />
+                    <asp:Button Text="DELETE" ID="btnsubmit2" CssClass="btn btn-primary" Width="170px" runat="server" BackColor="Red" ForeColor="Black" OnClick="btnsubmit2_Click" />
+                    <asp:Button Text="SEARCH" ID="btnsubmit3" CssClass="btn btn-primary" Width="170px" runat="server" BackColor="Yellow" ForeColor="Black" OnClick="btnsubmit3_Click" />
+                    <asp:Button Text="GET" ID="btnsubmit4" CssClass="btn btn-primary" Width="170px" runat="server" BackColor="#FF9900" ForeColor="Black" OnClick="btnsubmit4_Click" />
+               </div>
+                        <div style="width: 80%; margin: auto;">
+            <asp:GridView 
+    ID="GridView1" 
+    runat="server" 
+    AutoGenerateColumns="true"
+    GridLines="Both"
+    BorderStyle="Solid"
+    BorderWidth="1"
+    BorderColor="Black"
+    CellPadding="5"
+    CellSpacing="0"
+    HeaderStyle-BackColor="#CCCCCC"
+    HeaderStyle-Font-Bold="true" 
+/>
+            </div>
+               </div>
+            </div>
+            
+           </div>
+             </section>
+            </div>
+            
+              </div>
+
+            </section>
+        </section>
+
+</asp:Content>
---
+++ Registration_Form/Candidate_Registration.aspx.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using static System.Net.Mime.MediaTypeNames;
+
+
+namespace Registration_Form
+{
+    public partial class Candidate_Registration : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                LoadRecord();
+            }
+
+            /*if (!IsPostBack)
+            {
+                DataTable dt = new DataTable();
+                dt.Columns.Add("First Name");
+                dt.Columns.Add("Last Name");
+                dt.Columns.Add("Address");
+                dt.Columns.Add("DOB");
+                dt.Columns.Add("Mobile Number");
+                dt.Columns.Add("Email");
+
+                ViewState["RegistrationData"] = dt;
+            }*/
+        }
+        SqlConnection con = new SqlConnection("Data Source=localhost\\SQLEXPRESS02;Initial Catalog=registration;User ID=sa;Password=123456;Encrypt=False");
+
+        protected void btnsubmit_Click(object sender, EventArgs e)
+        {
+            con.Open();
+            SqlCommand cmd = new SqlCommand("INSERT INTO candidate_registration values('" + txtFirstName.Text + "','" + txtLastName.Text + "','" + txtAddress.Text + "','" + txtDOB.Text + "','" + txtEmail.Text + "','" + txtMobile.Text + "')", con);
+            cmd.ExecuteNonQuery();
+            con.Close();
+            ScriptManager.RegisterStartupScript(this, this.GetType(), "script", "alert('Successfully Inserted');", true);
+            LoadRecord();
+
+
+            /* DataTable dt = ViewState["RegistrationData"] as DataTable;
+
+             // Add new row
+             dt.Rows.Add(
+                 txtFirstName.Text,
+                 txtLastName.Text,
+                 txtAddress.Text,
+                 txtDOB.Text,
+                 txtMobile.Text,
+                 txtEmail.Text
+             );
+
+             // Update ViewState and GridView
+             ViewState["RegistrationData"] = dt;
+             GridView1.DataSource = dt;
+             GridView1.DataBind();
+
+             // Optional: Clear fields
+             txtFirstName.Text = txtLastName.Text = txtAddress.Text = txtDOB.Text = txtMobile.Text = txtEmail.Text = "";*/
+        }
+
+
+            void LoadRecord()
+            {
+                SqlCommand cd = new SqlCommand("Select * from candidate_registration", con);
+                SqlDataAdapter d = new SqlDataAdapter(cd);
+                DataTable dt = new DataTable();
+                d.Fill(dt);
+                GridView1.DataSource = dt;
+                GridView1.DataBind();
+
+            }
+
+        protected void btnsubmit1_Click(object sender, EventArgs e)
+        {
+            con.Open();
+            SqlCommand cmd = new SqlCommand("Update candidate_registration set fname = '"+txtFirstName.Text+"',lname = '"+txtLastName.Text+ "',address = '"+txtAddress.Text+"',dob = '"+txtDOB.Text+"',mobnum = '"+txtMobile.Text+"' where email = '"+txtEmail.Text+"'", con);
+            cmd.ExecuteNonQuery();
+            con.Close();
+            ScriptManager.RegisterStartupScript(this, this.GetType(), "script", "alert('Successfully Updated');", true);
+            LoadRecord();
+        }
+
+        protected void btnsubmit2_Click(object sender, EventArgs e)
+        {
+            con.Open();
+            SqlCommand cmd = new SqlCommand("Delete candidate_registration where email = '" + txtEmail.Text + "'", con);
+            cmd.ExecuteNonQuery();
+            con.Close();
+            ScriptManager.RegisterStartupScript(this, this.GetType(), "script", "alert('Successfully Deleted');", true);
+            LoadRecord();
+        }
+
+        protected void btnsubmit3_Click(object sender, EventArgs e)
+        {
+            SqlCommand cd = new SqlCommand("Select * from candidate_registration where email = '" + txtEmail.Text + "'", con);
+            SqlDataAdapter d = new SqlDataAdapter(cd);
+            DataTable dt = new DataTable();
+            d.Fill(dt);
+            GridView1.DataSource = dt;
+            GridView1.DataBind();
+        }
+
+        protected void btnsubmit4_Click(object sender, EventArgs e)
+        {
+            string dob = Request.QueryString["dob"];
+
+
+            con.Open();
+            SqlCommand cd = new SqlCommand("Select * from candidate_registration where email = '" + txtEmail.Text + "'", con);
+            SqlDataReader r = cd.ExecuteReader();
+            while (r.Read())
+            {
+                txtFirstName.Text=r.GetValue(0).ToString();
+                txtLastName.Text = r.GetValue(1).ToString();
+                txtAddress.Text = r.GetValue(2).ToString();
+                txtDOB.Text = Convert.ToDateTime(r.GetValue(3)).ToString("dd/MM/yyyy");
+                txtMobile.Text = r.GetValue(5).ToString();
+            }
+        }
+    }
+}
